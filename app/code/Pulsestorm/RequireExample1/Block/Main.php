d555d54679dfe511686abf39a9577a33ed65a563
Alan Storm <astorm@alanstorm.com>
2016-01-26 08:34:02 -0800

    Initial Commit with module

---
+++ app/code/Pulsestorm/RequireExample1/Block/Main.php
@@ -0,0 +1,6 @@
+<?php
+namespace Pulsestorm\RequireExample1\Block;
+class Main extends \Magento\Framework\View\Element\Template
+{
+    function _prepareLayout(){}
+}
---
+++ app/code/Pulsestorm/RequireExample1/Controller/Index/Index.php
@@ -0,0 +1,18 @@
+<?php
+namespace Pulsestorm\RequireExample1\Controller\Index;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $resultPageFactory;
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;        
+        return parent::__construct($context);
+    }
+    
+    public function execute()
+    {
+        return $this->resultPageFactory->create();  
+    }
+}
